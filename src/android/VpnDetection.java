b52833e7cac0ffe2319dd53a10d334310be82040
Pavan Madusha <pavan.madhusha@directfn.com>
2025-05-29 18:00:51 +0530

    Refactor For Android

--- src/android/VpnDetection.java
+++ src/android/VpnDetection.java
@@ -4,26 +4,39 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
+import android.os.Build;
 
-import org.apache.cordova.*;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
-public class VpnDetection extends CordovaPlugin {
+public class VPNDetection extends CordovaPlugin {
+
+    private Context context;
+
+    @Override
+    protected void pluginInitialize() {
+        // Ensure context is set early in lifecycle
+        this.context = this.cordova.getContext();
+    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("isVPNConnected".equals(action)) {
-            boolean isVpn = isVPNConnected();
-            callbackContext.success(isVpn ? 1 : 0);
+            this.cordova.getThreadPool().execute(() -> {
+                boolean isVpn = isVPNConnected();
+                callbackContext.success(isVpn ? 1 : 0);
+            });
             return true;
         }
         return false;
     }
 
     private boolean isVPNConnected() {
-        ConnectivityManager cm = (ConnectivityManager) cordova.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             Network activeNetwork = cm.getActiveNetwork();
             if (activeNetwork != null) {
                 NetworkCapabilities caps = cm.getNetworkCapabilities(activeNetwork);
