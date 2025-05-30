8fadf3e698d3dda081d660151b44c60b6aeec805
Pavan Madusha <pavan.madhusha@directfn.com>
2025-05-30 08:57:19 +0530

    Refactor Plugin

--- src/android/VpnDetection.java
+++ src/android/VpnDetection.java
@@ -1,47 +1,36 @@
 package cordova.android.vpndetection;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
-import android.os.Build;
-
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
-
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.net.NetworkInterface;
+import java.util.Collections;
+
 public class VPNDetection extends CordovaPlugin {
-
-    private Context context;
-
-    @Override
-    protected void pluginInitialize() {
-        // Ensure context is set early in lifecycle
-        this.context = this.cordova.getContext();
-    }
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if ("isVPNConnected".equals(action)) {
-            this.cordova.getThreadPool().execute(() -> {
-                boolean isVpn = isVPNConnected();
-                callbackContext.success(isVpn ? 1 : 0);
-            });
+        if (action.equals("isVPNConnected")) {
+            boolean vpnEnabled = isVPNConnected();
+            callbackContext.success(vpnEnabled ? 1 : 0); // send 1 if VPN is enabled, 0 otherwise
             return true;
         }
         return false;
     }
 
     private boolean isVPNConnected() {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Network activeNetwork = cm.getActiveNetwork();
-            if (activeNetwork != null) {
-                NetworkCapabilities caps = cm.getNetworkCapabilities(activeNetwork);
-                return caps != null && caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN);
+        try {
+            for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces())) {
+                if (networkInterface.isUp() && networkInterface.getInterfaceAddresses().size() > 0) {
+                    if ("tun0".equals(networkInterface.getName()) || "ppp0".equals(networkInterface.getName())) {
+                        return true; // VPN is active
+                    }
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return false;
     }
